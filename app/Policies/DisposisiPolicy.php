55f7455f35e249ab8245a40aa1f8b53dde384101
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-04-21 11:31:03 +0700

    feat(policy): implement or update security policy for

--- app/Policies/DisposisiPolicy.php
+++ app/Policies/DisposisiPolicy.php
@@ -1,108 +1,75 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Policies;
 
-use App\Models\User;
+use Illuminate\Foundation\Auth\User as AuthUser;
 use App\Models\Disposisi;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class DisposisiPolicy
 {
     use HandlesAuthorization;
-
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
+    
+    public function viewAny(AuthUser $authUser): bool
     {
-        return $user->can('view_any_disposisi');
+        return $authUser->can('ViewAny:Disposisi');
     }
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Disposisi $disposisi): bool
+    public function view(AuthUser $authUser, Disposisi $disposisi): bool
     {
-        return $user->can('view_disposisi');
+        return $authUser->can('View:Disposisi');
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
+    public function create(AuthUser $authUser): bool
     {
-        return $user->can('create_disposisi');
+        return $authUser->can('Create:Disposisi');
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Disposisi $disposisi): bool
+    public function update(AuthUser $authUser, Disposisi $disposisi): bool
     {
-        return $user->can('update_disposisi');
+        return $authUser->can('Update:Disposisi');
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Disposisi $disposisi): bool
+    public function delete(AuthUser $authUser, Disposisi $disposisi): bool
     {
-        return $user->can('delete_disposisi');
+        return $authUser->can('Delete:Disposisi');
     }
 
-    /**
-     * Determine whether the user can bulk delete.
-     */
-    public function deleteAny(User $user): bool
+    public function deleteAny(AuthUser $authUser): bool
     {
-        return $user->can('delete_any_disposisi');
+        return $authUser->can('DeleteAny:Disposisi');
     }
 
-    /**
-     * Determine whether the user can permanently delete.
-     */
-    public function forceDelete(User $user, Disposisi $disposisi): bool
+    public function restore(AuthUser $authUser, Disposisi $disposisi): bool
     {
-        return $user->can('force_delete_disposisi');
+        return $authUser->can('Restore:Disposisi');
     }
 
-    /**
-     * Determine whether the user can permanently bulk delete.
-     */
-    public function forceDeleteAny(User $user): bool
+    public function forceDelete(AuthUser $authUser, Disposisi $disposisi): bool
     {
-        return $user->can('force_delete_any_disposisi');
+        return $authUser->can('ForceDelete:Disposisi');
     }
 
-    /**
-     * Determine whether the user can restore.
-     */
-    public function restore(User $user, Disposisi $disposisi): bool
+    public function forceDeleteAny(AuthUser $authUser): bool
     {
-        return $user->can('restore_disposisi');
+        return $authUser->can('ForceDeleteAny:Disposisi');
     }
 
-    /**
-     * Determine whether the user can bulk restore.
-     */
-    public function restoreAny(User $user): bool
+    public function restoreAny(AuthUser $authUser): bool
     {
-        return $user->can('restore_any_disposisi');
+        return $authUser->can('RestoreAny:Disposisi');
     }
 
-    /**
-     * Determine whether the user can replicate.
-     */
-    public function replicate(User $user, Disposisi $disposisi): bool
+    public function replicate(AuthUser $authUser, Disposisi $disposisi): bool
     {
-        return $user->can('replicate_disposisi');
+        return $authUser->can('Replicate:Disposisi');
     }
 
-    /**
-     * Determine whether the user can reorder.
-     */
-    public function reorder(User $user): bool
+    public function reorder(AuthUser $authUser): bool
     {
-        return $user->can('reorder_disposisi');
+        return $authUser->can('Reorder:Disposisi');
     }
+
 }
