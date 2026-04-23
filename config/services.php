dd547d1b20554ab37b31738a3f735687577a93c8
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-04-23 17:14:05 +0700

    style

--- config/services.php
+++ config/services.php
@@ -36,10 +36,10 @@ return [
     ],
 
     'sipetra' => [
-        'client_id'     => env('SIPETRA_CLIENT_ID'),
+        'client_id' => env('SIPETRA_CLIENT_ID'),
         'client_secret' => env('SIPETRA_CLIENT_SECRET'),
-        'redirect'      => env('SIPETRA_REDIRECT_URI'),
-        'base_url'      => env('SIPETRA_BASE_URL'),
+        'redirect' => env('SIPETRA_REDIRECT_URI'),
+        'base_url' => env('SIPETRA_BASE_URL'),
         'scopes' => ['profile:read', 'email:read', 'identity:read', 'organization:read'],
     ],
 
