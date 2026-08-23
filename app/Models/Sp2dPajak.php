ab3e3b60d9df34870e0e9b969fb35d2c38a50203
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-08-23 10:26:16 +0700

    feat(sp2d): update Sp2dPajak model fields

--- app/Models/Sp2dPajak.php
+++ app/Models/Sp2dPajak.php
@@ -8,17 +8,24 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Sp2dPajak extends Model
 {
     protected $fillable = [
-        'rekap_id',
-        'jenis_pajak',
-        'jumlah_pajak',
+        'sp2d_rekap_id',
+        'npwp_nik',
+        'nama_pihak',
+        'kode_akun_pajak',
+        'nama_akun_pajak',
+        'dpp',
+        'nominal_pajak',
+        'no_drpp_kuitansi',
+        'ntpn_billing',
     ];
 
     protected $casts = [
-        'jumlah_pajak' => 'integer',
+        'dpp' => 'integer',
+        'nominal_pajak' => 'integer',
     ];
 
     public function rekap(): BelongsTo
     {
-        return $this->belongsTo(Sp2dRekap::class, 'rekap_id');
+        return $this->belongsTo(Sp2dRekap::class, 'sp2d_rekap_id');
     }
 }
